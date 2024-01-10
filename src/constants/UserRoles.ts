2cdce6f282e6f68f892f1ae5f95d52e0a6b69951
kulde <ku1de@icloud.com>
2024-01-10 23:57:12 +0100

    auth controller

---
+++ src/constants/UserRoles.ts
@@ -0,0 +1,7 @@
+import { Role } from "../models/Role";
+
+export const UserRoles = {
+   ADMIN: { id: 1, name: "admin" } as Role,
+   ARTIST: { id: 2, name: "artist" } as Role,
+   CUSTOMER: { id: 3, name: "customer" } as Role,
+};
