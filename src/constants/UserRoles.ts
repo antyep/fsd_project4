d2aebc9b9073c5885ecae3022e12fd5215af5744
kulde <ku1de@icloud.com>
2024-01-10 23:44:38 +0100

    types folder finished

--- src/constants/UserRoles.ts
+++
@@ -1,7 +0,0 @@
-import { Role } from "../models/Roles";
-
-export const UserRoles = {
-    ADMIN: { id= 1, name: "admin" }, as Role,
-    ARTIST: { id= 2, name: "artist" } as Role,
-    CUSTOMER: { id= 3, name: "customer"} as Role,
-};
