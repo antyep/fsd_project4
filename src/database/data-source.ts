8e1d78bd0b959c85a2a3558f992276277e333ccf
kulde <cids6x@gmail.com>
2024-01-13 16:49:57 +0100

    updated db credentials

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,28 +1,15 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-// import { CreateRoles1702301789294 } from "./migrations/01-CreateRoles";
-// import { CreateUsers1702301796616 } from "./migrations/02-CreateUsers";
-// import { CreateUsersRoles1702301805968 } from "./migrations/03-CreateUsersRoles";
-
-// -----------------------------------------------------------------------------
 
 export const AppDataSource = new DataSource({
-   type: "mysql",
-   host: "localhost",
-   port: 3307,
-   username: "root", //Cambio a datos correctos
-   password: "ReinodeIrlanda27250.", //Cambio a datos correctos
-   database: "project4-database-1", //Cambio a datos correctos
-   entities: [`${__dirname}/../models/**/*{.js,.ts}`],
-   migrations: [`${__dirname}/migrations/**/*{.js,.ts}`],
-   // migrations: [
-   //    CreateRoles1702301789294,
-   //    CreateUsers1702301796616,
-   //    CreateUsersRoles1702301805968,
-   // ],
-   // Estas migraciones cambiaran
-   synchronize: false,
-   logging: false,
+	type: "mysql",
+	host: "localhost",
+	port: 3306,
+	username: "user",
+	password: "password",
+	database: "db",
+	entities: [`${__dirname}/../models/**/*{.js,.ts}`],
+	migrations: [`${__dirname}/migrations/**/*{.js,.ts}`],
+	synchronize: false,
+	logging: false,
 });
-
-// console.log(`${__dirname}/migrations`);
