d891884220fb09affdc76364c0d1016ac62bf217
kulde <cids6x@gmail.com>
2024-01-13 16:49:58 +0100

    added migrations and roles

---
+++ src/database/migrations/1705157608908-CreateUserTable.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUserTable1705157608908 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: "users",
+				columns: [
+					{
+						name: "id",
+						type: "int",
+						isPrimary: true,
+						isGenerated: true,
+						generationStrategy: "increment",
+					},
+					{
+						name: "username",
+						type: "varchar",
+						length: "40",
+						isUnique: true,
+					},
+					{
+						name: "password_hash",
+						type: "varchar",
+						length: "255",
+					},
+					{
+						name: "email",
+						type: "varchar",
+						length: "255",
+						isUnique: true,
+					},
+					{
+						name: "is_admin",
+						type: "boolean",
+						default: false,
+					},
+				],
+			}),
+			true
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable("users");
+	}
+}
--- src/models/Role.ts
+++ src/models/Role.ts
@@ -1,37 +1,25 @@
-import {
-	Column,
-	Entity,
-	JoinTable,
-	ManyToMany,
-	PrimaryGeneratedColumn,
- } from "typeorm";
- import { User } from "./User";
- 
- @Entity("roles")
- export class Role {
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./User";
+
+@Entity("roles")
+export class Role {
 	@PrimaryGeneratedColumn()
 	id!: number;
- 
-	@Column()
-	name!: string; 
 
-    @Column()
-	createdAt!: Date;
-    
-    @Column()
-	updatedAt!: Date; 
- 
+	@Column()
+	name!: string;
+
 	@ManyToMany(() => User, (user: User) => user.roles)
 	@JoinTable({
-	   name: "roles",
-	   joinColumn: {
-		  name: "role_id",
-		  referencedColumnName: "id",
-       },
-	   inverseJoinColumn: {
-		  name: "user_id",
-		  referencedColumnName: "id",
-	}, 
+		name: "roles",
+		joinColumn: {
+			name: "role_id",
+			referencedColumnName: "id",
+		},
+		inverseJoinColumn: {
+			name: "user_id",
+			referencedColumnName: "id",
+		},
 	})
 	users?: User[];
- }
+}
