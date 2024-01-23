868cda06005d79dc4d323fa04ccc1a3862f85477
kulde <ku1de@icloud.com>
2024-01-23 23:57:02 +0100

    auth routes modified adding appointments

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { AuthController } from "../controllers/AuthController";
+import { auth } from "../middlewares/auth";
 
 const router = express.Router();
 const authController = new AuthController();
@@ -7,5 +8,11 @@ const authController = new AuthController();
 router.post("/register", authController.register);
 
 router.post("/login", authController.login);
+
+// router.post("/logout", authController.logout);
+
+router.get("/profile", auth, authController.getProfile)
+
+router.put("/profile", auth, authController.updateProfile)
 
 export default router;
