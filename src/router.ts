1f2a0f4e6dc6cc58403a24fe8bd0b938fe29b331
kulde <ku1de@icloud.com>
2024-01-23 23:58:51 +0100

    router modified adding appointments

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoutes from "./routes/users.routes";
 import authRoutes from "./routes/auth.routes";
+import appointments from "./routes/appointments.routes";
 
 const router = express.Router();
 
 router.use("/api/auth", authRoutes);
-router.use("/api/admin/users", userRoutes);
+router.use("/api/users", userRoutes);
+router.use("/api/appointments", appointments)
 
 export default router;
