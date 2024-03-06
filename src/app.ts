90257dee96e731a4227dc3d71d43f6f52434ef21
kulde <ku1de@icloud.com>
2024-03-06 19:49:21 +0100

    app ts modification for requests

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,20 @@ app.use(express.json());
 
 app.use(router);
 
+app.use((req, res, next) => {
+  res.header('Access-control-Allow-Origin', '*');
+  res.header(
+    'Accesss-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
 app.get('/api', (req, res) => {
     res.send('kd api')
   })
