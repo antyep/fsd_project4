ef499b8296681b2a4ac76002da26c0c3e7e3ec55
kulde <ku1de@icloud.com>
2024-03-06 20:44:05 +0100

    moving cors up to fix cors policy issue

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,14 @@
 import express, { Application } from "express";
 import router from "./router";
+import cors  from 'cors';
 
 const app: Application = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use(router);
-
-app.use((req, res, next) => {
-  res.header('Access-control-Allow-Origin', '*');
-  res.header(
-    'Accesss-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
-
 
 app.get('/api', (req, res) => {
     res.send('kd api')
